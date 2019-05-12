309629d023c41b7cd268197c6e8b8c92c63aa569
Daniel <danieloberlechner@hotmail.de>
2019-05-12 21:33:36 +0200

    index, script and vscode changes

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,7 @@ $(document).ready(function() {
         $(window).scrollTo($('#main'), 500);
     })
     document.getElementById('link3').addEventListener('click', function() {
-        $(window).scrollTo($('#functions'), 1000);
+        $(window).scrollTo($('#devices'), 1000);
     })
     document.getElementById('link4').addEventListener('click', function() {
         $(window).scrollTo($('#contact'), 1500);
